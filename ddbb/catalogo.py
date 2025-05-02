62bca80158d723d5b8bdf231b252454b9ce978f9
jchavarrias <chavarriasan.javier@gmail.com>
2025-05-02 18:04:13 +0200

    creo las tablas y la organización entre ellas

--- ddbb/catalogo.py
+++ ddbb/catalogo.py
@@ -1,44 +1,30 @@
+#importaciones
 from sqlalchemy import Column, ForeignKey, Integer, String, Table
 from sqlalchemy.sql.sqltypes import NullType
 from sqlalchemy.orm import relationship, DeclarativeBase
 from sqlalchemy.ext.declarative import declarative_base
-
+#declaración clase madre
 class Base (DeclarativeBase):
     pass
+
 metadata = Base.metadata
-
-
-class RecetaIngrediente(Base):
-    __tablename__ = 'receta_ingrediente'
-
+Base = declarative_base()
+#declaración tablas
+class Producto(Base):
+    __tablename__ = 'producto'
+    IDPRODUCTO = Column(Integer, primary_key=True, autoincrement=True)
+    nombre = Column(String(100))
+    categoria_producto = relationship("Catalogo", back_populates="producto")
+class Categoria(Base):
+    __tablename__ = 'categoria'
+    IDCATEGORIA = Column(Integer, primary_key=True, autoincrement=True)
+    nombre = Column(String(100))
+    categoria_producto = relationship("Catalogo",back_populates="categoria" )
+class Catalogo(Base):
+    __tablename__ = 'catalogo'
     id = Column(Integer, primary_key=True)
-    IDINGREDIENTE = Column(ForeignKey('ingrediente.IDINGREDIENTE'))
-    IDRECETA = Column(ForeignKey('receta.IDRECETA'))
-    CANTIDAD = Column(Integer, nullable=False)
-
-    ingrediente = relationship('Ingrediente')
-    receta = relationship('Receta')
-class Ingrediente(Base):
-    __tablename__ = 'ingrediente'
-
-    IDINGREDIENTE = Column(Integer, primary_key=True,autoincrement=True)
-    nombre = Column(String(100))
-    recetas = relationship("RecetaIngrediente",back_populates="ingrediente")
-
-
-
-class Receta(Base):
-    __tablename__ = 'receta'
-
-    IDRECETA = Column(Integer, primary_key=True,autoincrement=True)
-    nombre = Column(String(100))
-    ingrediente_receta  = relationship("RecetaIngrediente", back_populates="receta")
-
-
-t_sqlite_sequence = Table(
-    'sqlite_sequence', metadata,
-    Column('name', NullType),
-    Column('seq', NullType)
-)
-
-
+    IDPRODUCTO = Column(Integer, ForeignKey('producto.IDPRODUCTO'))
+    IDCATEGORIA = Column(Integer, ForeignKey('categoria.IDCATEGORIA'))
+    PRECIO = Column(Integer, nullable=False)
+    producto = relationship("Producto", back_populates="categoria_producto")
+    categoria = relationship("Categoria", back_populates="categoria_producto")
