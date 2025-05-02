6841a4ef104325c421f517f51349919491700f8d
jchavarrias <chavarriasan.javier@gmail.com>
2025-05-02 20:18:41 +0200

    añadimos un visualizador de tabla con rich para mejorar ux

--- listar_productos.py
+++ listar_productos.py
@@ -2,8 +2,11 @@ from catalogo import *
 from sqlalchemy.orm import sessionmaker
 import os
 from sqlalchemy import create_engine
+from rich.console import Console
+from rich.table import Table
 
 def listar_producto():
+    console = Console()
     basedir = os.path.abspath(os.path.dirname(__file__))
 
     engine = create_engine(
@@ -19,7 +22,17 @@ def listar_producto():
     session = Session()
 
     productos = session.query(Producto).all()
+    tabla = Table(title=" LISTA DE PRODUCTOS", show_header=True, header_style="bold blue")
+    tabla.add_column("ID", style="cyan", width=10)
+    tabla.add_column("Nombre", style="magenta")
+    tabla.add_column("Descripción", min_width=20)
+    tabla.add_column("Cantidad", justify="right", style="yellow")
 
     for producto in productos:
-         print(f"ID: {producto.id} | Nombre: {producto.nombre} | Descripción: {producto.descripcion} | Cantidad: {producto.cantidad}")
-
+        tabla.add_row(
+            str(producto.id),
+            producto.nombre,
+            producto.descripcion,
+            str(producto.cantidad)
+        )
+    console.print(tabla)
