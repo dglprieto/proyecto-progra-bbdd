987e457706e46a72df01c5afce3398bcda0c6e8e
dglprieto <dglprieto@gmail.com>
2025-05-02 19:15:41 +0200

    Estoy eliminando el producto

--- eliminar_producto.py
+++ eliminar_producto.py
@@ -1,12 +1,23 @@
-# eliminar_producto.py
-
-from creacion_engine import Session
-from producto import Producto
+from ddbb.catalogo import *
+from sqlalchemy.orm import sessionmaker
+import os
+from sqlalchemy import create_engine
 
 def eliminar_producto():
+    basedir = os.path.abspath(os.path.dirname(_file_))
+
+    engine = create_engine(
+        "sqlite:///" + os.path.join(basedir, 'catalogo.db'),
+        pool_size=5,
+        max_overflow=10,
+        pool_timeout=30,
+        echo=True
+    )
+
+    Session = sessionmaker(bind=engine)
     session = Session()
 
-    id_producto = input("Introduce el ID del producto que quieres eliminar: ")
+    id_producto = int(input("Introduce el ID del producto que quieres eliminar: "))
     producto = session.query(Producto).get(id_producto)
 
     if producto:
